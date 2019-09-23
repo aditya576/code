70e0d8f37d06ea4401b0fa10d4bb6b7138000005
aditya576 <55613768+aditya576@users.noreply.github.com>
2019-09-23 12:57:56 +0530

    Add files via upload

--- sine.cpp
+++ sine.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main()
 {
-    float i,n,x,term,sum=0;
+    float i,n,x,j,mins=-1,fact,term,sum=0;
     cout<<"Enter x and n:";
     cin>>x>>n;
     x=x*3.14/180;
-    term=x;
-    sum=x;
-    for(i=1;i<=n;i++)
+
+    for(i=1;i<=n;i+=2)
     {
-        term=term*(-1)*(2*i-1)*x*x/(2*i+1);
+        fact=1;
+        for(j=1;j<=i;j++)
+        {
+            fact*=j;
+        }
+        mins*=-1;
+        term=mins*pow(x,i)/fact;
         sum+=term;
     }
     cout<<"sin "<<x<<"="<<sum;
